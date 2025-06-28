99fbb236e25c0c9f6f1baec9d5e5bb0d097db5c4
GosuCode <gosucode1945@gmail.com>
2025-06-28 08:31:17 +0545

    Refactor code style for consistency and updated .gitignore

--- src/controller/indexController.ts
+++ src/controller/indexController.ts
@@ -6,14 +6,14 @@ const { readFile } = promises;
 
 export default async function indexController(fastify: FastifyInstance) {
   // GET /
-  fastify.get("/", async function (
-    _request: FastifyRequest,
-    reply: FastifyReply
-  ) {
-    const indexHtmlPath = resolve(__dirname, "../../static/index.html");
-    const indexHtmlContent = await readFile(indexHtmlPath);
-    reply
-      .header("Content-Type", "text/html; charset=utf-8")
-      .send(indexHtmlContent);
-  });
+  fastify.get(
+    "/",
+    async function (_request: FastifyRequest, reply: FastifyReply) {
+      const indexHtmlPath = resolve(__dirname, "../../static/index.html");
+      const indexHtmlContent = await readFile(indexHtmlPath);
+      reply
+        .header("Content-Type", "text/html; charset=utf-8")
+        .send(indexHtmlContent);
+    },
+  );
 }
